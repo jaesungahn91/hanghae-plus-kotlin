3755bd9483c0e9a57e5265339fb0c16e9cd3f0ba
jaesungahn91 <jaesungahn91@gmail.com>
2023-09-12 19:10:17 +0900

    first commit

---
+++ src/main/kotlin/io/github/ahnjs/hanghae/HanghaePlusKotlinApplication.kt
@@ -0,0 +1,11 @@
+package io.github.ahnjs.hanghae
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class HanghaePlusKotlinApplication
+
+fun main(args: Array<String>) {
+    runApplication<HanghaePlusKotlinApplication>(*args)
+}
---
+++ src/test/kotlin/io/github/ahnjs/hanghae/HanghaePlusKotlinApplicationTests.kt
@@ -0,0 +1,13 @@
+package io.github.ahnjs.hanghae
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class HanghaePlusKotlinApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
