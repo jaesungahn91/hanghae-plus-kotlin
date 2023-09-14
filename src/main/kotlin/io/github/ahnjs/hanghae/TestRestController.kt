266960f53801918331cef0525515927569adfa05
jaesungahn91 <jaesungahn91@gmail.com>
2023-09-14 19:20:01 +0900

    test

--- src/main/kotlin/io/github/ahnjs/hanghae/TestRestController.kt
+++ src/main/kotlin/io/github/ahnjs/hanghae/TestRestController.kt
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.RestController
 @RestController
 class TestRestController {
 
-    @GetMapping("/test")
+    @GetMapping("/")
     fun test() = Unit
 
 }
