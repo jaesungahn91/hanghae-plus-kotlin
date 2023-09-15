f29be79bc99825f2c51e57b237be048562a32dac
jaesungahn91 <jaesungahn91@gmail.com>
2023-09-15 21:34:33 +0900

    ci test

--- src/main/kotlin/io/github/ahnjs/hanghae/TestRestController.kt
+++ src/main/kotlin/io/github/ahnjs/hanghae/TestRestController.kt
@@ -7,5 +7,7 @@ import org.springframework.web.bind.annotation.RestController
 class TestRestController {
 
     @GetMapping("/")
-    fun test() = Unit
+    fun test(){
+        Math.abs(i)
+    }
 }
